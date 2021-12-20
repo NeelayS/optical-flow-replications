ffb791b1d32ba1f7a1ac782d7d2cb1d268ad6fe4
NeelayS <shahnh19@gmail.com>
2021-12-20 01:46:14 -0500

    Init

---
+++ train_flownets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=08:00:00
+#SBATCH --job-name=flownets-run1
+#SBATCH --partition=gpu
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --output=outs/flownets/run1.out
+
+module load cuda/10.2
+cd ..
+python flownets.py
