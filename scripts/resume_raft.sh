f8f6d4abbb0ec5bbcba84020223aa5716c542d12
NeelayS <shahnh19@gmail.com>
2021-12-20 15:20:25 -0500

    Minor change

--- scripts/resume_raft.sh
+++ scripts/resume_raft.sh
@@ -15,4 +15,4 @@ python train.py --train_cfg "./configs/raft_trainer.yaml" \
                 --log_dir "./logs/raft/run2" \
                 --ckpt_dir "./ckpts/raft/run2" \
                 --resume True \
-                --resume_ckpt "./ckpts/raft/run2/raft_epochs11.pth"
+                --resume_ckpt "./ckpts/raft/run2/raft_epochs13.pth"
