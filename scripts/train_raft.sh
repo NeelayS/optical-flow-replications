d69c77e920dda002049ac97a99db6bf2ad4fbf9c
NeelayS <shahnh19@gmail.com>
2022-01-03 10:25:56 -0500

    Added files for new experiments

--- scripts/train_raft.sh
+++ scripts/train_raft.sh
@@ -2,15 +2,16 @@
 
 #SBATCH --nodes=1
 #SBATCH --time=08:00:00
-#SBATCH --job-name=raft-run3
+#SBATCH --job-name=raft-run4
 #SBATCH --partition=gpu
 #SBATCH --mem=24G
 #SBATCH --gres=gpu:v100-sxm2:1
-#SBATCH --output=../outs/raft/run3.out
+#SBATCH --output=../outs/raft/run4.out
 
 module load cuda/10.2
 cd ../
 python train.py --train_cfg "./configs/raft_trainer.yaml" \
                 --model "RAFT" \
-                --log_dir "./logs/raft/run3" \
-                --ckpt_dir "./ckpts/raft/run3"
+                --log_dir "./logs/raft/run4" \
+                --ckpt_dir "./ckpts/raft/run4" \
+                # --model_cfg "./configs/models/raft_128.yaml" \
