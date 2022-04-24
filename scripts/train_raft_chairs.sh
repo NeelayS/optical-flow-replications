cf3cea1b88955626ca7676ab74b480a588d9ecb5
NeelayS <shahnh19@gmail.com>
2022-04-24 14:58:43 -0400

    Added scripts for new experiments

--- scripts/train_raft_chairs.sh
+++ scripts/train_raft_chairs.sh
@@ -5,16 +5,16 @@
 #SBATCH --partition=jiang
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:a6000:1
-#SBATCH --output=../../../../Share/optical_flow/replications/outs/raft/chairs.out
+#SBATCH --output=../../../../Share/optical_flow/replications/outs/raft/chairs-run2.out
 
 
 module load cuda/11.3
 cd ..
-python train.py --train_cfg "configs/raft_trainer.yaml" \
+python train.py --train_cfg "configs/raft_chairs_trainer.yaml" \
                 --model "RAFT" \
                 --model_cfg "configs/models/raft_default.yaml" \
-                --log_dir "../../../Share/optical_flow/replications/logs/raft" \
-                --ckpt_dir "../../../Share/optical_flow/replications/ckpts/raft" \
+                --log_dir "../../../Share/optical_flow/replications/logs/raft/run2/chairs" \
+                --ckpt_dir "../../../Share/optical_flow/replications/ckpts/raft/run2/chairs" \
                 --epochs 100 \
-                --batch_size 10 \
+                --batch_size 16 \
                 --device "0" 
--- scripts/train_raft_things.sh
+++ scripts/train_raft_things.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --time=72:00:00
+#SBATCH --time=96:00:00
 #SBATCH --job-name=raft-default-things
 #SBATCH --partition=jiang
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:a6000:1
-#SBATCH --output=../../../../Share/optical_flow/replications/outs/raft/things.out
+#SBATCH --output=../../../../Share/optical_flow/replications/outs/raft/things-run2.out
 
 
 module load cuda/11.3
@@ -13,8 +13,9 @@ cd ..
 python train.py --train_cfg "configs/raft_things_trainer.yaml" \
                 --model "RAFT" \
                 --model_cfg "configs/models/raft_default.yaml" \
-                --log_dir "../../../Share/optical_flow/replications/logs/raft" \
-                --ckpt_dir "../../../Share/optical_flow/replications/ckpts/raft" \
-                --epochs 100 \
-                --model_ckpt "../../../Share/optical_flow/replications/ckpts/raft/chairs/raft_best.pth" \
+                --log_dir "../../../Share/optical_flow/replications/logs/raft/run2/things" \
+                --ckpt_dir "../../../Share/optical_flow/replications/ckpts/raft/run2/things" \
+                --epochs 50 \
+                --model_ckpt "../../../Share/optical_flow/replications/ckpts/raft/run1/chairs/raft_best_final.pth" \
+                --batch_size 16 \
                 --device "0" 
